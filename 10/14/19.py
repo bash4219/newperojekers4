0ffd735d41692dafe892eb79ea1ceb2425e22ff1
bash4219 <kevin.bash18@gmail.com>
2019-10-22 16:09:49 -0500

    doing piy

--- 10/14/19.py
+++ 10/14/19.py
@@ -168,4 +168,46 @@ def montePi(numDarts):
     pi = inCircle / numDarts * 4
     return pi
 
-print(montePi(10000000000))
+print(montePi(100))
+
+import turtle
+turtle.speed(.5)
+def showMontePi(numDarts):
+    scn = turtle.Screen()
+    t = turtle.Turtle()
+
+    scn.setworldcoordinates(-2, -2, 2, 2)
+
+    t.penup()
+    t.goto(-1, 0)
+    t.pendown()
+    t.goto(1, 0)
+
+    t.penup()
+    t.goto(0, 1)
+    t.pendown()
+    t.goto(0, -1)
+
+    inCircle = 0
+    t.penup()
+
+    for i in range(numDarts):
+        x = random.randrange(-100, 100) / 100
+        y = random.randrange(-100, 100) / 100
+
+        distance = math.sqrt(x**2 + y**2)
+        t.goto(x, y)
+
+        if distance <= 1:
+            inCircle = inCircle + 1
+            t.color("blue")
+        else:
+            t.color("red")
+
+        t.dot()
+
+    pi = inCircle / numDarts * 4
+    scn.exitonclick()
+    return pi
+
+print(showMontePi(100))
